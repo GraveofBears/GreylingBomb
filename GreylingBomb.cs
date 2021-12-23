717e26b7e70dbdcdc181a63f78bef879799385af
GraveofBears <modf999@yahoo.com>
2021-12-23 13:45:55 -0500

    fixed and added update to item manager

--- GreylingBomb.cs
+++ GreylingBomb.cs
@@ -1,11 +1,5 @@
-﻿using System.IO;
-using System.Reflection;
-using BepInEx;
-using BepInEx.Configuration;
-using BepInEx.Logging;
-using HarmonyLib;
+﻿using BepInEx;
 using ItemManager;
-using ServerSync;
 using UnityEngine;
 
 namespace GreylingBomb
@@ -14,13 +8,13 @@ namespace GreylingBomb
     public class GreylingBomb : BaseUnityPlugin
     {
         private const string ModName = "GreylingBomb";
-        private const string ModVersion = "0.0.2";
+        private const string ModVersion = "0.0.3";
         private const string ModGUID = "org.bepinex.plugins.greylingbomb";
 
         public void Awake()
 
         {
-            Item GreylingBomb = new Item("greylingbomb", "GreylingBomb", "BoomLing");
+            Item GreylingBomb = new Item("greylingbomb", "GreylingBomb", "BoomLing");  //assetbundle name, Asset Name, Folder Contiaining AssetBundle
             GreylingBomb.Crafting.Add(CraftingTable.Forge, 3);
             GreylingBomb.RequiredItems.Add("Iron", 100);
             GreylingBomb.RequiredItems.Add("BlackMetal", 100);
@@ -29,9 +23,11 @@ namespace GreylingBomb
             GreylingBomb.RequiredUpgradeItems.Add("BlackMetal", 50);
             GreylingBomb.CraftAmount = 1;
 
-            GameObject sfx_greyling_explode = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "sfx_greyling_explode");
-
-
+            GameObject sfx_greyling_explode = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "sfx_greyling_explode"); //custom SFX attached to prefab
+            GameObject Greyling_Projectile = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "Greyling_Projectile"); //projected prefab
+            GameObject vfx_greyling_explode = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "vfx_greyling_explode"); //custom VFX attached to projected prefab
+            GameObject sfx_greyling_equip = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "sfx_greyling_equip"); //custom SFX attached to SE_Stat script
+            GameObject sfx_greyling_throw = ItemManager.PrefabManager.RegisterPrefab("greylingbomb", "sfx_greyling_throw"); //custom SFX attached to main prefab
 
         }
     }
