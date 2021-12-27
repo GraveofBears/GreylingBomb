f49f2b72dc436e84590d730fdb4502ecdc554817
GraveofBears <modf999@yahoo.com>
2021-12-27 09:45:14 -0500

    name change

--- Boomlings.cs
+++ Boomlings.cs
@@ -2,24 +2,24 @@
 using ItemManager;
 using UnityEngine;
 
-namespace GreylingBomb
+namespace Boomlings
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class GreylingBomb : BaseUnityPlugin
+    public class Boomlings : BaseUnityPlugin
     {
-        private const string ModName = "GreylingBomb";
-        private const string ModVersion = "0.0.7";
-        private const string ModGUID = "org.bepinex.plugins.greylingbomb";
+        private const string ModName = "Boomlings";
+        private const string ModVersion = "0.0.9";
+        private const string ModGUID = "org.bepinex.plugins.boomlings";
 
         public void Awake()
 
         {
+    
             Item GreylingBomb = new Item("greylingbomb", "GreylingBomb", "BoomLing");  //assetbundle name, Asset Name, Folder Contiaining AssetBundle
             GreylingBomb.Crafting.Add(CraftingTable.Forge, 3);
             GreylingBomb.RequiredItems.Add("DragonTear", 3);
-            GreylingBomb.RequiredItems.Add("Flametal ", 5);
+            GreylingBomb.RequiredItems.Add("Needle", 15);
             GreylingBomb.RequiredItems.Add("GoblinTotem", 2);
-            GreylingBomb.RequiredUpgradeItems.Add("Flametal", 5);
             GreylingBomb.RequiredUpgradeItems.Add("DragonTear", 3);
             GreylingBomb.RequiredUpgradeItems.Add("GoblinTotem", 2);
             GreylingBomb.CraftAmount = 1;
@@ -30,9 +30,8 @@ namespace GreylingBomb
             Item TrollBomb = new Item("greylingbomb", "TrollBomb", "BoomLing");  //assetbundle name, Asset Name, Folder Contiaining AssetBundle
             TrollBomb.Crafting.Add(CraftingTable.Forge, 3);
             TrollBomb.RequiredItems.Add("DragonTear", 3);
-            TrollBomb.RequiredItems.Add("Flametal ", 5);
+            TrollBomb.RequiredItems.Add("Needle", 15);
             TrollBomb.RequiredItems.Add("GoblinTotem", 2);
-            TrollBomb.RequiredUpgradeItems.Add("Flametal", 5);
             TrollBomb.RequiredUpgradeItems.Add("DragonTear", 3);
             TrollBomb.RequiredUpgradeItems.Add("GoblinTotem", 2);
             TrollBomb.CraftAmount = 1;
